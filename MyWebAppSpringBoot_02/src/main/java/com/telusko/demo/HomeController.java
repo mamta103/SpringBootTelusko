74eda5be4a209b8775a457e92271be8114bc2464
Mamta Kumari <32190445+mamta103@users.noreply.github.com>
2018-09-14 19:14:52 +0530

    #5. Web App using Spring Boot
    Web App using Spring Boot

---
+++ MyWebAppSpringBoot_02/src/main/java/com/telusko/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.telusko.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("home")
+	public String home() {
+		System.out.println("Hi ");
+		return "home.jsp";
+	}
+}
---
+++ MyWebAppSpringBoot_02/src/main/java/com/telusko/demo/MyWebAppSpringBoot02Application.java
@@ -0,0 +1,12 @@
+package com.telusko.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAppSpringBoot02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppSpringBoot02Application.class, args);
+	}
+}
