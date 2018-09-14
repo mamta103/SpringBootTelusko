d3390da85c3f19a8789ade83003bdc980c10d516
Mamta Kumari <32190445+mamta103@users.noreply.github.com>
2018-09-14 23:26:06 +0530

    #9. Web App using Spring Boot | Model Object
    Web App using Spring Boot | Model Object

---
+++ MyWebAppSpringBoot_02/src/main/java/com/telusko/demo/Alien.java
@@ -0,0 +1,33 @@
+package com.telusko.demo;
+
+public class Alien {
+
+	private String aname;
+	private int aid;
+	private String tech;
+
+	public String getAname() {
+		return aname;
+	}
+
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+
+	public int getAid() {
+		return aid;
+	}
+
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+
+	public String getTech() {
+		return tech;
+	}
+
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+
+}
--- MyWebAppSpringBoot_02/src/main/java/com/telusko/demo/HomeController.java
+++ MyWebAppSpringBoot_02/src/main/java/com/telusko/demo/HomeController.java
@@ -2,14 +2,19 @@ package com.telusko.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("home")
-	public String home() {
-		System.out.println("Hi ");
-		return "home.jsp";
+	public ModelAndView home(Alien alien) {
+		// Just Creating ModelAndView to make code easy removed all HttpSession and all these thing :)
+		ModelAndView mv = new ModelAndView();
+		// Setting MvelAndView Object
+		mv.addObject("alienObject", alien);
+		// Setting MvelAndView page
+		mv.setViewName("home");
+		return mv;
 	}
 }
