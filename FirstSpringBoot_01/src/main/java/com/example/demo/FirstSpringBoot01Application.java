44060617b43365ab17ec1f99a74eeeaf7b67214c
Mamta Kumari <32190445+mamta103@users.noreply.github.com>
2018-09-14 16:34:23 +0530

    Initial commit

---
+++ FirstSpringBoot_01/src/main/java/com/example/demo/FirstSpringBoot01Application.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBoot01Application.class, args);
+		System.out.println("Hello");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.err.println();
+	}
+}
